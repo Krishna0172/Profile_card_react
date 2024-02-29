f31f9b80bb6117de5b12e62c2731d4fc7836a503
Krishna0172 <krishkrishna2710@gmail.com>
2024-02-29 12:09:32 +0530

    last commit

---
+++ src/UserCard.js
@@ -0,0 +1,96 @@
+import PropType  from "prop-types";
+
+const userData = [
+    {
+        name:'Krishna R',
+        city:'Coimbatore',
+        description : 'Full Stack Developer',
+        skills:['Nodejs','Express','MongoDB','GraphQL','React','Redux','Nextjs','React Native','Flutter','Dart','Java','Python'],
+        online:true,
+        profile: "images/krishna.jpg ",
+    },
+    // {
+    //     name:'Krishna',
+    //     city:'Coimbatore',
+    //     description : 'Backend Developer',
+    //     skills:[ 'Node','Sentiment Analysis','Web Scrapping','Amazon Aws','Python', 'JavaScript', 'C Programming', 'C++', 'SQL', 'React'],
+    //     online:true,
+    //     profile: "images/tiger.jpg",
+    // },
+    // {
+    //     name:'James',
+    //     city:'New -York',
+    //     description : 'Full Stack Developer',
+    //     skills:['Nodejs','Express','MongoDB','GraphQL','React','Redux','Nextjs','React Native','Flutter','Dart','Java','Python'],
+    //     online:false,
+    //     profile: "images/cat.jpg",
+    // },
+    // {
+    //     name:'Lio',
+    //     city:'Coimbatore',
+    //     description : 'Full Stack Developer',
+    //     skills:[ 'Node','Sentiment Analysis','Web Scrapping','Amazon Aws','Python', 'JavaScript', 'C Programming', 'C++', 'SQL', 'React'],
+    //     online:false,
+    //     profile: "images/lion.jpg",
+    // }
+]
+
+function User(props) {
+    return (
+        <div className="card-container">
+            <span className={props.online ? "pro online" : "pro offline"}>
+                {props.online ? "ONLINE" : "OFFLINE"}
+            </span>
+            <img src={props.profile} alt={props.name} className="img" />
+            <h3>{props.name}</h3>
+            <h3>{props.city}</h3>
+            <p>{props.description}</p>
+            <div className="buttons">
+                <button className="primary">Message</button>
+                <button className="primary outline">Following</button>
+            </div>
+            <div className="skills">
+                <h6>Skills</h6>
+                <ul>
+                    {props.skills.map((skill, index) => (
+                        <li key={index}>{skill}</li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+function UserCard() {
+    return (
+        <>
+            {userData.map((user, index) => (
+                <User
+                    key={index} // Use a unique identifier as the key
+                    name={user.name}
+                    city={user.city}
+                    description={user.description}
+                    skills={user.skills}
+                    online={user.online}
+                    profile={user.profile}
+                />
+            ))}
+        </>
+    );
+}
+
+export default UserCard;
+
+
+User.prototype = {
+    name: PropType.string.isRequired,
+    city: PropType.string.isRequired,
+    description: PropType.string.isRequired,
+    skills: PropType.array.isRequired,
+    online: PropType.bool.isRequired,
+    profile: PropType.string.isRequired,
+
+}
+// <div>
+// <User name='Krishna' city = 'Coimbatore' description = 'Full Stack Developer'skills = {['Sentiment Analysis','Web Scrapping','Amazon Aws','Python', 'JavaScript', 'C Programming', 'C++', 'SQL', 'React', 'Node']} online = {true} />
+// </div>
